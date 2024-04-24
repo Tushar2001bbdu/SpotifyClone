584b1d03fb06db7a713ae1458f1c90d73232f421
Tushar2001bbdu <tusharkumaargupta032@gmail.com>
2024-04-24 21:55:31 +0530

    3

--- src/Components/login.js
+++ src/Components/login.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function login() {
     function handleClick(){
         const clientId="ce6449902069416998100e188a2e749a"
-        const redirecturl="ht"
+        const redirecturl="https://tushar2001bbdu.github.io/SpotifyClone/"
        
         const scope=['user-read-email',
         'user-read-private',
